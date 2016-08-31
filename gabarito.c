6739d49d158334be53246ba5bf0f1911962c062f
Victor Wichmann Raposo <vwraposo@gmail.com>
2016-08-31 11:00:55 -0300

    Template para os programas

---
+++ gabarito.c
@@ -0,0 +1,29 @@
+/* -*- coding: iso-latin-1-unix; -*- */
+/* DECLARO QUE SOU O UNICO AUTOR E RESPONSAVEL POR ESTE PROGRAMA.
+// TODAS AS PARTES DO PROGRAMA, EXCETO AS QUE FORAM FORNECIDAS
+// PELO PROFESSOR OU COPIADAS DO LIVRO OU DAS BIBLIOTECAS DE
+// SEDGEWICK OU ROBERTS, FORAM DESENVOLVIDAS POR MIM.  DECLARO
+// TAMBEM QUE SOU RESPONSAVEL POR TODAS AS COPIAS DESTE PROGRAMA
+// E QUE NAO DISTRIBUI NEM FACILITEI A DISTRIBUICAO DE COPIAS.
+// 
+// Autor: 
+// Numero USP: 
+// Sigla: 
+// Data: 2015-mm-dd
+// 
+// Este arquivo faz parte da tarefa ????
+// da disciplina MAC0121.
+// 
+////////////////////////////////////////////////////////////// */
+
+/* //// Leia e depois apague este bloco /////////////////////////
+//
+// 0. O nome deste arquivo e "gabarito". Mude o nome para algo
+//    mais apropriado.
+// 1. Escreva um comentario no inicio para dizer _o_que_
+//    o programa faz.
+// 2. Antes de cada funcao, escreva um comentario para dizer
+//    _o_que_ a funcao faz.
+// 3. Nao escreva nenhum outro comentario.
+//
+////////////////////////////////////////////////////////////// */
